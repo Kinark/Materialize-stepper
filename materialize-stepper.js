4ecf2cff7fac8e2109c8856ca87067b32fb2fa1b
Kinark <igormarcossi@gmail.com>
2016-11-17 09:01:43 -0200

    v2.0
    A lot of changes. Check changelog.md

--- materialize-stepper.js
+++ materialize-stepper.js
@@ -1,6 +1,6 @@
 /* Materializecss Stepper - By Kinark 2016
 // https://github.com/Kinark/Materialize-stepper
-// JS v1.2.3
+// JS v2.0
 */
 
 $.validator.setDefaults({
@@ -21,6 +21,11 @@ $.validator.setDefaults({
    }
 });
 
+$.fn.getActiveStep  = function() {
+   active = this.find('.step.active');
+   return $(this.children('.step:visible')).index($(active))+1;
+};
+
 $.fn.activateStep  = function() {
    $(this).addClass("step").stop().slideDown(function(){$(this).css({'height':'auto', 'margin-bottom': ''});});
 };
@@ -39,13 +44,25 @@ $.fn.showError  = function(error) {
 };
 
 $.fn.activateFeedback  = function() {
-   active = this.find('.step.active').find('.step-content');
-   return active.prepend('<div class="wait-feedback"> <div class="preloader-wrapper active"> <div class="spinner-layer spinner-blue"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-red"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-yellow"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-green"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div></div></div>');
+   active = this.find('.step.active:not(.feedbacking)').addClass('feedbacking').find('.step-content');
+   active.prepend('<div class="wait-feedback"> <div class="preloader-wrapper active"> <div class="spinner-layer spinner-blue"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-red"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-yellow"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-green"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div></div></div>');
 };
 
 $.fn.destroyFeedback  = function() {
-   active = this.find('.step.active');
-   return active.find('.step-content').find('.wait-feedback').remove();
+   active = this.find('.step.active.feedbacking');
+   if(active) {
+      active.removeClass('feedbacking');
+      active.find('.step-content').find('.wait-feedback').remove();
+   }
+   return true;
+};
+
+$.fn.resetStepper  = function(step) {
+   if(!step) step = 1;
+   form = $(this).closest('form');
+   $(form)[0].reset();
+   Materialize.updateTextFields();
+   return $(this).openStep(step);
 };
 
 $.fn.nextStep = function(ignorefb) {
@@ -76,14 +93,45 @@ $.fn.prevStep = function() {
 };
 
 $.fn.openStep = function(step) {
+   $this = this;
    step_num = step - 1;
    step = this.find('.step:visible:eq('+step_num+')');
    if(step.hasClass('active')) return;
    active = this.find('.step.active');
-   active.find('.step-content').find('.wait-feedback').remove();
-   active.removeClass('active').find('.step-content').stop().slideUp();
-   step.removeClass('done').addClass('active').find('.step-content').slideDown();
-   this.trigger('stepchange').trigger('step'+(step_num+1));
+   prev_active = next = $(this.children('.step:visible')).index($(active));
+   order = step_num > prev_active ? 1 : 0;
+   if(active.hasClass('feedbacking')) $this.destroyFeedback();
+   active.closeAction(order);
+   step.openAction(order, function(){
+      $this.trigger('stepchange').trigger('step'+(step_num+1));
+      if(step.data('event')) $this.trigger(step.data('event'));
+   });
+};
+
+$.fn.closeAction = function(order, callback) {
+   closable = this.removeClass('active').find('.step-content');
+   if(!this.closest('ul').hasClass('horizontal')) {
+      closable.stop().slideUp(300,"easeOutQuad", callback);
+   } else {
+      if(order==1) {
+         closable.animate({left: '-100%'},function(){closable.css({display: 'none', left: '0%'}, callback);});
+      } else {
+         closable.animate({left: '100%'},function(){closable.css({display: 'none', left: '0%'}, callback);});
+      }
+   }
+};
+
+$.fn.openAction = function(order, callback) {
+   openable = this.removeClass('done').addClass('active').find('.step-content');
+   if(!this.closest('ul').hasClass('horizontal')) {
+      openable.slideDown(300,"easeOutQuad", callback);
+   } else {
+      if(order==1) {
+         openable.css({left: '100%', display: 'block'}).animate({left: '0%'}, callback);
+      } else {
+         openable.css({left: '-100%', display: 'block'}).animate({left: '0%'}, callback);
+      }
+   }
 };
 
 $.fn.activateStepper = function() {
@@ -94,7 +142,7 @@ $.fn.activateStepper = function() {
       method = (method ? method : "GET");
       action = (action ? action : "?");
       $stepper.wrap( '<form action="'+action+'" method="'+method+'"></div>' );
-      $stepper.find('li.step.active .step-content').slideDown('normal');
+      $stepper.find('li.step.active').openAction(1);
 
       $stepper.on("click", '.step:not(.active)', function () {
          object = $($stepper.children('.step:visible')).index($(this));
@@ -114,6 +162,12 @@ $.fn.activateStepper = function() {
       }).on("click", '.previous-step', function (e) {
          e.preventDefault();
          $stepper.prevStep();
+      }).on("click", "button:submit:not(.next-step, .previous-step)", function (e) {
+         e.preventDefault();
+         form = $stepper.closest('form');
+         if(form.valid()) {
+            form.submit();
+         }
       });
    });
 };
