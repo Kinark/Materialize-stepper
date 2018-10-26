abbc7486d25dd43797b5ceea452c5c17323fda6e
Kinark <igormarcossi@gmail.com>
2018-10-26 10:40:53 -0300

    Updating eslint stuff. Now using eslint:recommended preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
    parser: 'babel-eslint',
+   extends: 'eslint:recommended',
+   env: {
+      browser: true,
+      node: true,
+      jasmine: true
+   },
    rules: {
       semi: 2,
       strict: 0
